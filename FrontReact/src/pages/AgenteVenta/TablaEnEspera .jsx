800a2a2cc73ae8a49d38d5b59a43ec53d78b429a
RichartM <20233tn230@utez.edu.mx>
2025-04-09 00:15:49 -0600

    Alert

--- FrontReact/src/pages/AgenteVenta/TablaEnEspera .jsx
+++ FrontReact/src/pages/AgenteVenta/TablaEnEspera .jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Table, Button } from "react-bootstrap";
 import styled from "styled-components";
 import { BiSolidUserVoice } from "react-icons/bi";
+import Swal from "sweetalert2";
 
 const StyledTable = styled(Table)`
   th {
@@ -42,33 +43,60 @@ const StyledTable = styled(Table)`
 `;
 
 const TablaEnEspera = ({ autos = [], onAprobar }) => {
-  console.log("Insaniiiiisimos",autos)
-  console.log("..................................................................................")
+  const handleAtender = (autoId) => {
+    Swal.fire({
+      title: '¿Atender compra?',
+      text: "¿Estás seguro de que deseas atender esta compra del cliente?",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#018180',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, atender',
+      cancelButtonText: 'Cancelar',
+      background: '#fff',
+      iconColor: '#018180',
+      customClass: {
+        confirmButton: 'swal-confirm-button',
+        cancelButton: 'swal-cancel-button'
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onAprobar(autoId);
+        Swal.fire(
+          '¡Atendido!',
+          'La compra ha sido atendida correctamente.',
+          'success'
+        );
+      }
+    });
+  };
+
   return (
-    <StyledTable striped  hover responsive>
+    <StyledTable striped hover responsive>
       <thead>
         <tr>
           <th>Modelo</th>
           <th>Marca</th>
-          <th>Matricula</th>
+          <th>Cliente</th>
           <th>Correo</th>
           <th>Precio (MXN)</th>
-          
           <th>Acciones</th>
         </tr>
       </thead>
       <tbody>
         {autos.map((auto, i) => (
           <tr key={i}>
-            <td>{auto.vehiculo.modelo}</td>
-            <td>{auto.vehiculo.marca.nombre}</td>
-            <td>{auto.cliente.name+" "+auto.cliente.lastname}</td>
-            
-            <td>{auto.cliente.email}</td>
-            <td>${auto.vehiculo.precio?.toLocaleString()}</td>
+            <td>{auto.vehiculo?.modelo || 'N/A'}</td>
+            <td>{auto.vehiculo?.marca?.nombre || 'N/A'}</td>
+            <td>{auto.cliente?.name ? `${auto.cliente.name} ${auto.cliente.lastname}` : 'N/A'}</td>
+            <td>{auto.cliente?.email || 'N/A'}</td>
+            <td>${auto.vehiculo?.precio?.toLocaleString() || '0'}</td>
             <td>
               <div className="btn-wrapper">
-                <Button className="btn-atender" onClick={() => onAprobar(auto.id)}>
+                <Button 
+                  className="btn-atender" 
+                  onClick={() => handleAtender(auto.id)}
+                >
                   <BiSolidUserVoice size={16} />
                   Atender
                 </Button>
